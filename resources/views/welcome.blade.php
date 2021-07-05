120584ea9551b051511a8eba96edcc5c7b040260
Michel <michel.berigo@federalst.com.br>
2021-07-05 11:42:56 -0300

    Removendo rotas de login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,12 +10,12 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-        <script src="{{ asset('js/welcome.js') }}" defer></script>
+        <script src="{{ asset('/js/app.js') }}" defer></script>
+        <script src="{{ asset('/js/welcome.js') }}" defer></script>
     
         <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('/css/welcome.css') }}" rel="stylesheet">
     </head>
 
     <body>
